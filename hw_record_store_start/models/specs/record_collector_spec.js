c9879a279b6d81ce47ad04144e6f2f6b2e3e9bfe
Mike-Heneghan <m.heneghan@live.co.uk>
2018-05-20 12:43:18 +0100

    Majority of record collector tests written and set to pending.

--- hw_record_store_start/models/specs/record_collector_spec.js
+++ hw_record_store_start/models/specs/record_collector_spec.js
@@ -24,10 +24,60 @@ describe('RecordCollector', function(){
     recordCollector = new RecordCollector();
   });
 
-
-
+xit('should start with no funds', function(){
+  const actual = recordCollector.getFunds();
+  assert.strictEqual(actual, 0)
 });
 
+xit('should be able to add funds', function(){
+  recordCollector.addFunds(5000);
+  const actual = recordCollector.getFunds();
+  assert.strictEqual(actual, 5000);
+});
+
+xit('should be able to remove funds', function(){
+  recordCollector.addFunds(5000);
+  recordCollector.removeFunds(500);
+  const actual = recordCollector.getFunds();
+  assert.strictEqual(actual, 4500);
+});
+
+xit('should start with an empty collection of records', function(){
+  const actual = recordCollector.getCollectionSize();
+  assert.strictEqual(actual, 0);
+})
+
+xit('should be able to add a record to it\'s collection', function(){
+  recordCollector.addRecord(record1);
+  const actual = recordCollector.getRecords();
+  const testCase = [record1];
+  assert.deepStrictEqual(actual, testCase);
+});
+
+xit('should be able to find a record by title', function(){
+  recordCollector.addRecord(record1);
+  recordCollector.addRecord(record2);
+  const actual1 = recordCollector.findRecordByTitle('Neon Bible');
+  const actual2 = recordCollector.findRecordByTitle('Their Greatest Hits 1971 - 1975');
+  assert.deepStrictEqual(actual1, [record2]);
+  assert.deepStrictEqual(actual2, [record1]);
+});
+
+xit('should be able to remove a record from it\'s collection', function(){
+  recordCollector.addRecord(record1);
+  recordCollector.addRecord(record2);
+  recordCollector.removeRecord(record1);
+  const actual = recordCollector.getRecords();
+  assert.deepStrictEqual(actual, [record2]);
+});
+
+xit('should be able to buy a record if there is sufficient funds')
+
+xit('should be able to sell a record if ti has the record')
+});
+
+
+// should be able to sell a record if it has the record
 
 
 
